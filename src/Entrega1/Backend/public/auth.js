9287ae6a48e02eb61ae9fd754713f82dbdac73b6
eric-bittu <eric.silva2@edu.fecap.br>
2025-05-15 21:02:30 -0300

    Subindo arquivos do yanko

--- src/Entrega1/Backend/public/auth.js
+++ src/Entrega1/Backend/public/auth.js
@@ -1,5 +1,5 @@
 //LOGIN
-document.getElementById('loginForm')?.addEventListener('submit', async (e) =>{
+document.getElementById('loginForm')?.adEventListener('submit', async (e) =>{
     e.preventDefault()
     const username = document.getElementById('username').value
     const password = document.getElementById('password').value
